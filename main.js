cd1b3beab5833b62953680a9ab90946cf33ec2d7
Timothy Negron <timothy.negron@codeimmersives.com>
2021-03-12 08:25:09 -0500

    Adds functions.

--- main.js
+++ main.js
@@ -1,8 +1,46 @@
 // Your code here.
 
+function getFirstName (person) {
+  return person.firstName;
+}
 
+function getLastName (person) {
+  return person.lastName;
+}
 
+function getFullName (person) {
+  return person.firstName + ' ' + person.lastName;
+}
 
+function setFirstName (person, firstName) {
+  person.firstName = firstName;
+}
+
+function setAge (person, age) {
+  person.age = age;
+}
+
+function giveBirthday (person) {
+  if (person.age){
+    person.age += 1;
+  }else {
+    person.age = 1;
+  }
+}
+
+function marry (person1, person2) {
+  person1.married = true;
+  person1.spouseName = getFullName(person2);
+  person2.married = true;
+  person2.spouseName = getFullName(person1);
+}
+
+function divorce (person1, person2) {
+  person1.married = false;
+  delete person1.spouseName;
+  person2.married = false;
+  delete person2.spouseName;
+}
 
 // Our code here. Don't touch!
 if (typeof getFirstName === 'undefined') {
